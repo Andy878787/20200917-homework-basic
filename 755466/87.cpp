91c2ace101b0f4d6d91be39051eeb4707417821d
Andy謝 <a755466@stdmail.nssh.ntpc.edu.tw>
2020-10-01 12:12:16 +0800

    29544 755466 謝奕宏
    29544 755466 謝奕宏

--- 755466/87.cpp
+++ 755466/87.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
 using namespace std;
-
 int main() {
     int num;
-    cout << "請輸入你的數字: ";
+    cout << "請輸入數字:";
     cin >> num;
+
     if (num % 3 == 0) {
-      cout << "這是三的倍數";
+        cout << "是3的倍數\n";
     }
     else {
-        cout << "這不是三的倍數";
+        cout << "不是3的倍數";
     }
 }
